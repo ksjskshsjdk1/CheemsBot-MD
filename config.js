36a5802b7bff250583405b7ec74fa8a9b4db399e
ksjskshsjdk1 <103904360+ksjskshsjdk1@users.noreply.github.com>
2022-04-19 13:46:25 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['201288455338','201288455338'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['201288455338'] //another owner number
+global.premium = ['201288455338'] //premium number
+global.pengguna = 'Youssef-Al Soltan' //username
+global.botnma = 'Al Soltan-Bot' //bot name
+global.ownernma = '🦄Youssef Al Soltan' //owner name
+global.packname = 'Bot Al Soltan' //sticker package name
+global.author = 'By youssef al soltan' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
